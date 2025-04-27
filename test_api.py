80aeb200054fb0a16fa0234b0f7deb6ca22344ab
yeweiyi <yeweiyi@bytedance.com>
2025-04-27 11:40:56 +0800

    param check

--- test_api.py
+++ test_api.py
@@ -6,15 +6,25 @@ PLAY_URL = 'http://localhost:8000/play'
 
 # 测试 POST 请求
 def test_play_post():
-    data = {
+    data1 = {
         'game': 'example_game',
         'uid': 'example_uid',
         'branch': 'example_branch',
         'language': 'zh',
         'content': 'example_content'
     }
-    response = requests.post(PLAY_URL, json=data)
-    assert response.status_code == 200
+    response = requests.post(PLAY_URL, json=data1)
+    assert response.json()["status_code"] == 200
+    
+    data2 = {
+        'game': 'example_game',
+        'branch': 'example_branch',
+        'uid': '',
+        'content': '你是谁'
+    }
+    response = requests.post(PLAY_URL, json=data2)
+
+    assert response.json()["status_code"] == 400
 
 def test_mission():
     data = {
